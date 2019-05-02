ad56ab7d02b50ae93f6fa5eac486e855452258db
emperorhan <iny27@naver.com>
2019-05-03 00:57:41 +0900

    feat: implement erc20 in eos contract

---
+++ eos/erctoken/ecrio.erctoken.cpp
@@ -0,0 +1,209 @@
+#include "ecrio.erctoken.hpp"
+
+namespace eosio {
+ACTION erctoken::issue( name to, asset quantity, string memo ) {
+    require_auth( get_self() );
+
+    auto sym = quantity.symbol;
+    check( sym.is_valid(), "invalid symbol name" );
+    check( memo.size() <= 256, "memo has more than 256 bytes" );
+
+    check( quantity.is_valid(), "invalid quantity" );
+    check( quantity.amount > 0, "must issue positive quantity" );
+
+    check( quantity.symbol == _stat_state.supply.symbol, "symbol precision mismatch" );
+
+    _stat_state.supply += quantity;
+
+    add_balance( to, quantity, get_self() );
+
+    if( to != get_self() ) {
+        SEND_INLINE_ACTION( *this, transfer, { {get_self(), "active"_n} },
+                            { get_self(), to, quantity, memo }
+        );
+    }
+}
+
+ACTION erctoken::burn( name account, asset quantity, string memo ) {
+    require_auth( account );
+
+    check( quantity.is_valid(), "invalid quantity" );
+    check( quantity.amount > 0, "must burn positive quantity" );
+    check( quantity.symbol == _stat_state.supply.symbol, "symbol precision mismatch" );
+    check( memo.size() <= 256, "memo has more than 256 bytes" );
+
+    sub_balance( account, quantity );
+    _stat_state.supply -= quantity;
+}
+
+ACTION erctoken::burnfrom( name burner, name owner, asset quantity, string memo ) {
+    require_auth( burner );
+    check( burner != owner, "cannot burn self" );
+    check( is_account( owner ), "owner account does not exist");
+
+    require_recipient( burner );
+    require_recipient( owner );
+
+    check( quantity.is_valid(), "invalid quantity" );
+    check( quantity.amount > 0, "must be positive quantity" );
+    check( quantity.symbol == _stat_state.supply.symbol, "symbol precision mismatch" );
+    check( memo.size() <= 256, "memo has more than 256 bytes" );
+
+    allowances allws( get_self(), owner.value );
+    auto itr = allws.require_find( burner.value, "burner is not exist" );
+    check( itr->balance.amount >= quantity.amount, "burner does not have enough allowed amount" );
+    allws.modify( itr, same_payer, [&]( auto& a ) {
+        a.balance -= quantity;
+    });
+    sub_balance( owner, quantity );
+    _stat_state.supply -= quantity;
+}
+
+ACTION erctoken::transfer( name         from,
+                           name         to,
+                           asset        quantity,
+                           string       memo ) {
+    check( from != to, "cannot transfer to self" );
+    require_auth( from );
+    check( is_account( to ), "to account does not exist");
+
+    require_recipient( from );
+    require_recipient( to );
+
+    check( quantity.is_valid(), "invalid quantity" );
+    check( quantity.amount > 0, "must transfer positive quantity" );
+    check( quantity.symbol == _stat_state.supply.symbol, "symbol precision mismatch" );
+    check( memo.size() <= 256, "memo has more than 256 bytes" );
+
+    auto payer = has_auth( to ) ? to : from;
+
+    sub_balance( from, quantity );
+    add_balance( to, quantity, payer );
+}
+
+void erctoken::sub_balance( name owner, asset value ) {
+    accounts from_acnts( get_self(), owner.value );
+
+    const auto& from = from_acnts.get( value.symbol.code().raw(), "no balance object found" );
+    check( from.balance.amount >= value.amount, "overdrawn balance" );
+
+    from_acnts.modify( from, owner, [&]( auto& a ) {
+        a.balance -= value;
+    });
+}
+
+void erctoken::add_balance( name owner, asset value, name ram_payer ) {
+    accounts to_acnts( get_self(), owner.value );
+    auto to = to_acnts.find( value.symbol.code().raw() );
+    if( to == to_acnts.end() ) {
+        to_acnts.emplace( ram_payer, [&]( auto& a ){
+            a.balance = value;
+        });
+    } else {
+        to_acnts.modify( to, same_payer, [&]( auto& a ) {
+            a.balance += value;
+        });
+    }
+}
+
+ACTION erctoken::approve( name owner, name spender, asset quantity ) {
+    require_auth( owner );
+
+    check( quantity.is_valid(), "invalid quantity" );
+    check( quantity.amount > 0, "must issue positive quantity" );
+    check( quantity.symbol == _stat_state.supply.symbol, "symbol precision mismatch" );
+    
+    allowances allws( get_self(), owner.value );
+    auto it = allws.find( spender.value );
+    check( it == allws.end(), "spender already exist" );
+    allws.emplace( owner, [&]( auto& a ){
+        a.spender = spender;
+        a.balance = quantity;
+    });
+}
+
+ACTION erctoken::transferfrom( name spender, name from, name to, asset quantity, string memo ) {
+    require_auth( spender );
+    check( from != to, "cannot transfer to self" );
+    check( is_account( from ), "from account does not exist");
+    check( is_account( to ), "to account does not exist");
+
+    require_recipient( from );
+    require_recipient( to );
+
+    check( quantity.is_valid(), "invalid quantity" );
+    check( quantity.amount > 0, "must transfer positive quantity" );
+    check( quantity.symbol == _stat_state.supply.symbol, "symbol precision mismatch" );
+    check( memo.size() <= 256, "memo has more than 256 bytes" );
+
+    allowances allws( get_self(), from.value );
+    auto it = allws.require_find( spender.value, "spender is not exist" );
+    check( it->balance.symbol == quantity.symbol, "symbol precision mismatch" );
+    check( it->balance.amount >= quantity.amount, "spender does not have enough allowed amount" );
+
+    auto payer = has_auth( to ) ? to : spender;
+
+    sub_balance( from, quantity );
+    add_balance( to, quantity, payer );
+
+    allws.modify( it, same_payer, [&]( auto& a ){
+        a.balance -= quantity;
+    });
+}
+
+ACTION erctoken::incallowance( name owner, name spender, asset quantity ) {
+    require_auth( owner );
+
+    check( quantity.is_valid(), "invalid quantity" );
+    check( quantity.amount > 0, "must issue positive quantity" );
+    check( quantity.symbol == _stat_state.supply.symbol, "symbol precision mismatch" );
+
+    allowances allws( get_self(), owner.value );
+    auto it = allws.require_find( spender.value, "spender is not registed" );
+    allws.modify( it, same_payer, [&]( auto& a ) {
+        a.balance += quantity;
+    });
+}
+
+ACTION erctoken::decallowance( name owner, name spender, asset quantity ) {
+    require_auth( owner );
+
+    check( quantity.is_valid(), "invalid quantity" );
+    check( quantity.amount > 0, "must issue positive quantity" );
+    check( quantity.symbol == _stat_state.supply.symbol, "symbol precision mismatch" );
+
+    allowances allws( get_self(), owner.value );
+    auto it = allws.require_find( spender.value, "spender is not registed" );
+    allws.modify( it, same_payer, [&]( auto& a ) {
+        a.balance -= quantity;
+    });
+}
+
+ACTION erctoken::open( name owner, const symbol& symbol, name ram_payer ) {
+    require_auth( ram_payer );
+
+    auto sym_code_raw = symbol.code().raw();
+
+    check( _stat_state.supply.symbol == symbol, "symbol precision mismatch" );
+
+    accounts acnts( get_self(), owner.value );
+    auto it = acnts.find( sym_code_raw );
+    if( it == acnts.end() ) {
+        acnts.emplace( ram_payer, [&]( auto& a ){
+            a.balance = asset{0, symbol};
+        });
+    }
+}
+
+ACTION erctoken::close( name owner, const symbol& symbol ) {
+    require_auth( owner );
+    accounts acnts( get_self(), owner.value );
+    auto it = acnts.find( symbol.code().raw() );
+    check( it != acnts.end(), "Balance row already deleted or never existed. Action won't have any effect." );
+    check( it->balance.amount == 0, "Cannot close because the balance is not zero." );
+    acnts.erase( it );
+}
+
+} // namespace eosio
+
+EOSIO_DISPATCH( eosio::erctoken, (issue)(burn)(burnfrom)(transfer)(approve)(transferfrom)(incallowance)(decallowance)(open)(close) )
---
+++ eos/erctoken/ecrio.erctoken.hpp
@@ -0,0 +1,97 @@
+#pragma once
+
+#include <eosiolib/asset.hpp>
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/singleton.hpp>
+
+#include <string>
+
+using namespace eosio;
+
+namespace eosio{
+    using std::string;
+
+    CONTRACT erctoken : public contract {
+        public:
+            erctoken( name self, name first_receiver, datastream<const char*> ds )
+            : contract( self, first_receiver, ds ), _stat( self, self.value ) {
+                _stat_state = _stat.exists() ? _stat.get() : currency_stat{};
+            }
+
+            ~erctoken() {
+                _stat.set( _stat_state, get_self() );
+            }
+
+            ACTION issue( name to, asset quantity, string memo );
+
+            ACTION burn( name account, asset quantity, string memo );
+
+            ACTION burnfrom( name burner, name owner, asset quantity, string memo );
+
+            ACTION transfer( name from, name to, asset quantity, string memo );
+        
+            ACTION approve( name owner, name spender, asset quantity );
+
+            ACTION transferfrom( name spender, name from, name to, asset quantity, string  memo );
+
+            ACTION incallowance( name owner, name spender, asset quantity );
+
+            ACTION decallowance( name owner, name spender, asset quantity );
+
+            ACTION open( name owner, const symbol& symbol, name ram_payer );
+
+            ACTION close( name owner, const symbol& symbol );
+
+            static asset get_supply( name token_contract_account ){
+                stat_singleton _stat( token_contract_account, token_contract_account.value );
+                const auto& st = _stat.get();
+                return st.supply;
+            }
+
+            static asset get_balance( name token_contract_account, name owner, symbol_code sym_code ){
+                accounts accountstable( token_contract_account, owner.value );
+                const auto& ac = accountstable.get( sym_code.raw() );
+                return ac.balance;
+            }
+
+            using issue_action = eosio::action_wrapper<"issue"_n, &erctoken::issue>;
+            using burn_action = eosio::action_wrapper<"burn"_n, &erctoken::burn>;
+            using burnfrom_action = eosio::action_wrapper<"burnfrom"_n, &erctoken::burnfrom>;
+            using transfer_action = eosio::action_wrapper<"transfer"_n, &erctoken::transfer>;
+            using approve_action = eosio::action_wrapper<"approve"_n, &erctoken::approve>;
+            using transferfrom_action = eosio::action_wrapper<"transferfrom"_n, &erctoken::transferfrom>;
+            using incallowance_action = eosio::action_wrapper<"incallowance"_n, &erctoken::incallowance>;
+            using decallowance_action = eosio::action_wrapper<"decallowance"_n, &erctoken::decallowance>;
+            using open_action = eosio::action_wrapper<"open"_n, &erctoken::open>;
+            using close_action = eosio::action_wrapper<"close"_n, &erctoken::close>;
+
+        private:
+            TABLE account {
+                asset    balance;
+                uint64_t primary_key()const { return balance.symbol.code().raw(); }
+            };
+
+            struct [[eosio::table("stat")]] currency_stat {
+                asset    supply = asset{0, symbol(symbol_code("TEST"), 5)};
+            };
+
+            TABLE allowance {
+                name     spender;
+                asset    balance;
+
+                uint64_t primary_key()const { return spender.value; }
+            };
+
+            typedef eosio::multi_index< "accounts"_n, account > accounts;
+            typedef eosio::multi_index< "allowances"_n, allowance > allowances;
+            typedef eosio::singleton< "stat"_n, currency_stat > stat_singleton;
+
+            stat_singleton _stat;
+
+            currency_stat  _stat_state;
+
+            void sub_balance( name owner, asset value );
+            void add_balance( name owner, asset value, name ram_payer );
+    };
+
+} // namespace eosio
